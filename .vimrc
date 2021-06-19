9e61b77e416c8d8b2a81dce28286cf4158008753
technuma <technuma@gmail.com>
2021-06-19 11:30:16 +0900

    add vim-plug, nerdtree

--- .vimrc
+++ .vimrc
@@ -1,3 +1,9 @@
+call plug#begin()
+Plug 'scrooloose/nerdtree'
+nnoremap <C-e> :NERDTreeToggle<CR>
+let NERDTreeShowHidden=1
+call plug#end()
+
 syntax on
 set autoindent
 set tabstop=2 shiftwidth=2 expandtab
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln .vimrc ~/.vimrc
 ln .tmux.conf ~/.tmux.conf
 ln .gitconfig ~/.gitconfig
